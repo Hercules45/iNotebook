d6099c8e2c2e40c75e60539515faa8489f6ac91c
Hercules45 <106345809+Hercules45@users.noreply.github.com>
2023-09-30 23:35:49 +0530

    First Commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,11 @@ import NoteState from './context/notes/NoteState'
 import Alert from './components/Alert';
 import SignUp from './components/Signup';
 import Login from './components/Login';
-
+import NoteItems from './components/NoteItems';
+import Map from './components/Map';
+import EditNote from './components/EditNote';
+import Test from './components/Test';
+import UpdateNote from './components/UpdateNote';
 function App() {
   
   const [alert, setAlert]=useState(null);
@@ -25,19 +29,38 @@ function App() {
 }
 
   return (
-    <div className='app'>
+    <div className="app">
       <NoteState>
         <BrowserRouter>
-          <Navbar />
+         
           <Alert alert={alert} />
-     
-          <div className='container my-4'>
+          <div className="container my-4 ">
             <Routes>
-              <Route path="/notes" element={<Notes showAlert={showAlert} />} />
-              <Route path="/" element={<AddNote showAlert={showAlert} />} />
-              <Route path="/about" element={<About showAlert={showAlert}/>} />
+              <Route
+                path="/notes/:id"
+                element={<Notes showAlert={showAlert} />}
+              />
+              <Route
+                path="/addnote/:id"
+                element={<AddNote showAlert={showAlert} />}
+              />
+              <Route path="/about" element={<About showAlert={showAlert} />} />
               <Route path="/login" element={<Login showAlert={showAlert} />} />
-              <Route path="/signup" element={<SignUp showAlert={showAlert}/>} />
+              <Route path="/" element={<SignUp showAlert={showAlert} />} />
+              <Route path="/test" element={<Test showAlert={showAlert} />} />
+              <Route
+                path="/noteitems"
+                element={<NoteItems showAlert={showAlert} />}
+              />
+              <Route path="/map/:id" element={<Map showAlert={showAlert} />} />
+              <Route
+                path="/editnote"
+                element={<EditNote showAlert={showAlert} />}
+              />
+              <Route
+                path="/updatenote"
+                element={<UpdateNote showAlert={showAlert} />}
+              />
             </Routes>
           </div>
         </BrowserRouter>
