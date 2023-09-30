383f77349383acdbd51db90a85fa8f8f0053d291
Hercules45 <106345809+Hercules45@users.noreply.github.com>
2023-09-30 22:51:48 +0530

    First Commit

--- backend/index.js
+++ backend/index.js
@@ -1,43 +1,35 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const connectToMongo = require('./database ');
-const { errorHandler, notFound }= require('./middleware/error.js');
-const path= require('path');
-var cors = require('cors');
-
-dotenv.config();
-
-connectToMongo();
-
-
+const express = require("express");
+const mongoose = require("mongoose");
+// const route = require("./routes/route");
+var cors = require("cors");
 const app = express();
+const route = require("./routes/route");
+// const { errorHandler, notFound } = require("./middleware/error.js");
+// const path = require("path");
+
+// const dotenv = require("dotenv");
 app.use(cors());
+//mongodb+srv://syedkhajafaizuddin786123:<password>@cluster0.g3lt2bq.mongodb.net/?retryWrites=true&w=majority
+// mongodb+srv://syedkhajafaizuddin786123:<password>@cluster0.vdslk4v.mongodb.net/?retryWrites=true&w=majority
+//mongodb+srv://syedkhajafaizuddin786123:<password>@cluster0.vdslk4v.mongodb.net/?retryWrites=true&w=majority
+mongoose.connect(
+  "mongodb+srv://syedkhajafaizuddin786123:password1212@cluster0.xbyhg40.mongodb.net/Inote?retryWrites=true&w=majority",
 
-app.use(express.json());// to accept json data
-
-//Available Routes
-app.use('/api/auth', require('./routes/auth'))
-app.use('/api/notes', require('./routes/notes'))
-
-// --------------------------deployment------------------------------
-
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "frontend", "build")));
-
-//   app.get("*", (req, res) =>
-//     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-//   );
-// } else {
-//   app.get("/", (req, res) => {
-//     res.send("API is running..");
-//   });
-// }
-// --------------------------deployment------------------------------
-
-// Error Handling middlewares
-app.use(notFound);
-app.use(errorHandler);
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, console.log(`iNotebook backend listening on port ${PORT}`))
+  {
+    useNewUrlParser: true,
+    // useFindAndModify: false,
+    useUnifiedTopology: true,
+  }
+);
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully");
+});
+app.use("/", route);
+app.listen(8000, () => {
+  console.log("server started");
+});
+// getNotes()
+// notes()
+// kjnkbv zkjvb zvnl
