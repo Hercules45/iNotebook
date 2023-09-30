988967a49157bae30927b1c860bd2870bf54ee69
Hercules45 <106345809+Hercules45@users.noreply.github.com>
2023-09-30 22:39:01 +0530

    First Commit

--- backend/models/Note.js
+++ backend/models/Note.js
@@ -1,29 +1,23 @@
-const mongoose = require('mongoose' ) ;
-const { Schema } = mongoose;
-
-
-const noteSchema = new Schema({
-    user:{
-      type: mongoose.Schema.Types.ObjectId,
-      ref:"user"
-    },
-    title: {
-        type: String,
-        required: true,
-    },
-    description: {
-        type: String,
-        required: true
-    },
-    tag: {
-        type: String,
-        default: "General"
-    },
-    date:{
-        type:Date,
-        default: Date.now
-    }
-  
+const { default: mongoose } = require("mongoose");
+const notes23 = mongoose.Schema({
+  user: {
+    type: String,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: { type: String, required: true },
+  tag: {
+    type: String,
+    default: "General",
+  },
+  createddate: {
+    type: Date,
+  },
+  updateddate:{
+    type:Date,
+  }
 });
-
-module.exports=  mongoose.model('note', noteSchema)
+const Notes23 = mongoose.model("Notes23", notes23)
+module.exports=Notes23
